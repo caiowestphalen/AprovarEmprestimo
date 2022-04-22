b61edb5cdf613ccead453caddd87a024cbb8fba6
caiowestphalen <caiowestphalen@hotmail.com>
2022-04-22 10:55:40 -0300

    Update ex036.py

--- ex036.py
+++ ex036.py
@@ -1,4 +1,5 @@
-# Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.
+# Programa que calcula seu salário e aprova ou não o empréstimo para compra de casa.
+# O valor da parcela não pode passar 30% do salário
 
 casa = float(input('Digite o valor da casa: R$'))
 salario = float(input('Digite quanto você recebe por mês: R$'))
